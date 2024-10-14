8bd081a4a71a37c676e819bce422cac663623fd8
miguelaarrones <miguelaarrones@icloud.com>
2024-10-14 21:53:00 +0200

    Added health UI for the player and the enemy

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthSystem : MonoBehaviour
 {
     [SerializeField] private float maxHealth;
+    [SerializeField] private RectTransform background;
+    [SerializeField] private Image healthBar;
+    [SerializeField] private Vector3 positionOffset = new Vector3(-0.5f, 0.5f, 0.0f);
 
     private float currentHealth;
 
@@ -14,10 +18,17 @@ public class HealthSystem : MonoBehaviour
         currentHealth = maxHealth;
     }
 
+    private void Update()
+    {
+        background.position = transform.position + positionOffset;
+    }
+
     public float GetCurrentHealth() => currentHealth;
 
     public void DecreaseCurrentHealth(float amount)
     {
         currentHealth -= amount;
+
+        healthBar.fillAmount = currentHealth / maxHealth;
     }
 }
